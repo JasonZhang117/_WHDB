aac029d7d5f84e6c5b2b9bef982774dbdc2668fa
JasonZhang117 <695881709@qq.com>
2020-03-25 15:10:56 +0800

    20200325-15-whdb

--- A_dbms/views/v_agree.py
+++ A_dbms/views/v_agree.py
@@ -278,32 +278,6 @@ def agree_scan_counter(request, agree_id, counter_id):  # 查看合同
     return render(request, 'dbms/agree/agree-scan.html', locals())
 
 
-# -------------------------抵押申请预览-------------------------#
-@login_required
-@authority
-@agree_right
-def mortgage_app(request, agree_id, warrant_id): #抵押申请预览
-    current_url_name = resolve(request.path).url_name  # 获取当前URL_NAME
-    authority_list = request.session.get('authority_list')  # 获取当前用户的所有权限
-    menu_result = MenuHelper(request).menu_data_list()
-
-    agree_obj = models.Agrees.objects.get(id=agree_id)
-    warrant_obj = models.Warrants.objects.get(id=warrant_id)
-
-    AGREE_TYP_D = models.Agrees.AGREE_TYP_D  # 担保公司合同类型
-    AGREE_TYP_X = models.Agrees.AGREE_TYP_X  # 小贷公司合同类型
-
-    agree_amount_cn = convert(agree_obj.agree_amount)  # 转换为金额大写
-    agree_amount_str = amount_s(agree_obj.agree_amount)  # 元转换为万元并去掉小数点后面的零
-    agree_amount_y = amount_y(agree_obj.agree_amount)  # 元转换为万元并去掉小数点后面的零
-    agree_term_cn = convert_num(agree_obj.agree_term)  # 合同期限转大写
-    agree_copy_cn = convert_num(agree_obj.agree_copies)
-
-    UN, ADD, CNB = un_dex(agree_obj.agree_typ)  # 不同合同种类下主体适用
-
-
-    return render(request, 'dbms/agree/preview-mortgage_app.html', locals())
-
 # -------------------------合同预览-------------------------#
 @login_required
 @authority
@@ -325,6 +299,38 @@ def agree_preview(request, agree_id):
     agree_copy_cn = convert_num(agree_obj.agree_copies)
 
     UN, ADD, CNB = un_dex(agree_obj.agree_typ)  # 不同合同种类下主体适用
+
+    if agree_obj.agree_typ in [22, ]:  # (22, 'D-公司保函'),
+        page_home_y_y = '申请人（乙方）'
+        page_home_y_j = '担保人（甲方）'
+    elif agree_obj.agree_typ in [21, ]:  # (21, 'D-分离式保函'),
+        page_home_y_y = '乙方'
+        page_home_y_j = '甲方'
+    else:
+        page_home_y_y = '被担保人（乙方）'
+        page_home_y_j = '担保人（甲方）'
+
+    notarization_typ = False  # 是否公证
+    if agree_obj.agree_typ in [1, 2, 3, 4, 21, 22, 23]:
+        agree_copy_jy_cn = convert_num(agree_obj.agree_copies - 2)
+    else:
+        notarization_typ = True
+        agree_copy_jy_cn = convert_num(agree_obj.agree_copies - 3)
+    '''利率（费率）处理'''
+    agree_rate_cn_q = ''
+    try:
+        rate_b = True
+        single_quota_rate = float(agree_obj.agree_rate)
+        charge = round(agree_obj.agree_amount * single_quota_rate / 100, 2)
+        agree_rate_cn_q = convert_num(float(agree_obj.agree_rate))  # 合同利率转换为千分之，大写
+        agree_rate_p = round(((20 - float(agree_obj.agree_rate)) / 30 * 10), 4)
+        agree_rate_w = convert_num_4(agree_rate_p)
+        charge_cn = convert(charge)
+    except ValueError:
+        rate_b = False
+        single_quota_rate = agree_obj.agree_rate
+        agree_rate_cn_q = agree_obj.agree_rate
+        agree_rate_w = '叁点叁叁叁叁'
 
     return render(request, 'dbms/agree/preview-agree.html', locals())
 
@@ -543,3 +549,30 @@ def result_preview(request, agree_id, result_id):
     result_detail = result_obj.result_detail
 
     return render(request, 'dbms/agree/preview-result.html', locals())
+
+
+# -------------------------抵押申请预览-------------------------#
+@login_required
+@authority
+@agree_right
+def mortgage_app(request, agree_id, warrant_id): #抵押申请预览
+    current_url_name = resolve(request.path).url_name  # 获取当前URL_NAME
+    authority_list = request.session.get('authority_list')  # 获取当前用户的所有权限
+    menu_result = MenuHelper(request).menu_data_list()
+
+    agree_obj = models.Agrees.objects.get(id=agree_id)
+    warrant_obj = models.Warrants.objects.get(id=warrant_id)
+
+    AGREE_TYP_D = models.Agrees.AGREE_TYP_D  # 担保公司合同类型
+    AGREE_TYP_X = models.Agrees.AGREE_TYP_X  # 小贷公司合同类型
+
+    agree_amount_cn = convert(agree_obj.agree_amount)  # 转换为金额大写
+    agree_amount_str = amount_s(agree_obj.agree_amount)  # 元转换为万元并去掉小数点后面的零
+    agree_amount_y = amount_y(agree_obj.agree_amount)  # 元转换为万元并去掉小数点后面的零
+    agree_term_cn = convert_num(agree_obj.agree_term)  # 合同期限转大写
+    agree_copy_cn = convert_num(agree_obj.agree_copies)
+
+    UN, ADD, CNB = un_dex(agree_obj.agree_typ)  # 不同合同种类下主体适用
+
+
+    return render(request, 'dbms/agree/preview-mortgage_app.html', locals())
