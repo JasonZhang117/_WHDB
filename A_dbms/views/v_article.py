297ce400917fb90e5e9fb311986d1bb63cd23ff3
JasonZhang <695881709@qq.com>
2020-07-31 09:07:43 +0800

    20200731-09-wxxd

--- A_dbms/views/v_article.py
+++ A_dbms/views/v_article.py
@@ -10,8 +10,9 @@ from django.db.utils import IntegrityError
 from django.db import transaction
 from django.db.models import Avg, Min, Sum, Max, Count
 from django.urls import resolve
-from _WHDB.views import (MenuHelper, authority, article_right, article_list_screen, UNX, UND,
-                         convert_str, amount_s, amount_y, un_dex, convert_num, convert)
+from _WHDB.views import (MenuHelper, authority, article_right,
+                         article_list_screen, UNX, UND, convert_str, amount_s,
+                         amount_y, un_dex, convert_num, convert)
 
 
 # -----------------------------项目列表------------------------------#
@@ -41,25 +42,30 @@ def article(request, *args, **kwargs):  # 项目列表
     '''筛选条件'''
     article_state_list = models.Articles.ARTICLE_STATE_LIST  # 筛选条件
     article_state_list_dic = list(
-        map(lambda x: {'id': x[0], 'name': x[1]}, article_state_list))
+        map(lambda x: {
+            'id': x[0],
+            'name': x[1]
+        }, article_state_list))
     # 列表或元组转换为字典并添加key[{'id': 1, 'name': '待反馈'}, {'id': 2, 'name': '已反馈'}]
     '''筛选'''
-    article_list = models.Articles.objects.filter(
-        **kwargs).select_related('custom', 'director', 'assistant', 'control')
+    article_list = models.Articles.objects.filter(**kwargs).select_related(
+        'custom', 'director', 'assistant', 'control')
     article_list = article_list_screen(article_list, request)  # 项目筛选
     '''搜索'''
     search_key = request.GET.get('_s')
     if search_key:
-        search_fields = ['article_num', 'custom__name', 'custom__short_name', 'director__name',
-                         'assistant__name', 'control__name']
+        search_fields = [
+            'article_num', 'custom__name', 'custom__short_name',
+            'director__name', 'assistant__name', 'control__name'
+        ]
         q = Q()
         q.connector = 'OR'
         for field in search_fields:
             q.children.append(("%s__contains" % field, search_key.strip()))
         article_list = article_list.filter(q)
     article_acount = article_list.count()  # 信息数目
-    balance = article_list.aggregate(Sum('article_balance'))[
-        'article_balance__sum']  # 在保余额
+    balance = article_list.aggregate(
+        Sum('article_balance'))['article_balance__sum']  # 在保余额
     '''分页'''
     paginator = Paginator(article_list, 19)
     page = request.GET.get('page')
@@ -92,37 +98,59 @@ def article_scan(request, article_id):  # 项目预览
         '-inv_date')  # 客户补调列表
     '''ARTICLE_STATE_LIST = [(1, '待反馈'), (2, '已反馈'), (3, '待上会'), (4, '已上会'), (5, '已签批'),
                           (51, '已放款'), (52, '已放完'), (55, '已解保'), (61, '待变更'), (99, '已注销')]'''
-    SHOW_SUM_LIST = [4, 5, 51, 52, 55, 61, ]  # 显示纪要的项目状态列表
+    SHOW_SUM_LIST = [
+        4,
+        5,
+        51,
+        52,
+        55,
+        61,
+    ]  # 显示纪要的项目状态列表
     form_date = {
-        'custom_id': article_obj.custom.id, 'product_id': article_obj.product.id,
-        'renewal': article_obj.renewal, 'process_id': article_obj.process.id,
-        'augment': article_obj.augment, 'credit_term': article_obj.credit_term,
+        'custom_id': article_obj.custom.id,
+        'product_id': article_obj.product.id,
+        'renewal': article_obj.renewal,
+        'process_id': article_obj.process.id,
+        'augment': article_obj.augment,
+        'credit_term': article_obj.credit_term,
         'director_id': article_obj.director.id,
-        'assistant_id': article_obj.assistant.id, 'control_id': article_obj.control.id,
-        'article_date': str(article_obj.article_date)}
+        'assistant_id': article_obj.assistant.id,
+        'control_id': article_obj.control.id,
+        'article_date': str(article_obj.article_date)
+    }
     form_article_add_edit = forms.ArticlesAddForm(
         initial=form_date)  # 项目变更form
     if feedbac_list:
         form_date = {
-            'propose': feedbac_list[0].propose, 'analysis': feedbac_list[0].analysis,
-            'suggestion': feedbac_list[0].suggestion}
+            'propose': feedbac_list[0].propose,
+            'analysis': feedbac_list[0].analysis,
+            'suggestion': feedbac_list[0].suggestion
+        }
         form_feedback = forms.FeedbackAddForm(initial=form_date)  # 添加项目反馈form
     else:
         form_feedback = forms.FeedbackAddForm()  # 添加项目反馈form
     '''ARTICLE_STATE_LIST = [(1, '待反馈'), (2, '已反馈'), (3, '待上会'), (4, '已上会'), (5, '已签批'),
                           (51, '已放款'), (52, '已放完'), (55, '已解保'), (61, '待变更'), (99, '已注销')]'''
     if article_obj.article_state in [1, 2, 3, 4]:
-        form_date = {'renewal': article_obj.renewal, 'augment': article_obj.augment,
-                     'sign_date': str(datetime.date.today())}
+        form_date = {
+            'renewal': article_obj.renewal,
+            'augment': article_obj.augment,
+            'sign_date': str(datetime.date.today())
+        }
         form_article_sign = forms.ArticlesSignForm(
             initial=form_date)  # 项目签批form
     else:
         form_date = {
-            'summary_num': article_obj.summary_num, 'sign_type': article_obj.sign_type, 'renewal': article_obj.renewal,
-            'augment': article_obj.augment, 'credit_amount': article_obj.custom.credit_amount,
+            'summary_num': article_obj.summary_num,
+            'sign_type': article_obj.sign_type,
+            'renewal': article_obj.renewal,
+            'augment': article_obj.augment,
+            'credit_amount': article_obj.custom.credit_amount,
             'rcd_opinion': article_obj.rcd_opinion,
-            'convenor_opinion': article_obj.convenor_opinion, 'sign_detail': article_obj.sign_detail,
-            'sign_date': str(article_obj.sign_date)}
+            'convenor_opinion': article_obj.convenor_opinion,
+            'sign_detail': article_obj.sign_detail,
+            'sign_date': str(article_obj.sign_date)
+        }
         form_article_sign = forms.ArticlesSignForm(
             initial=form_date)  # 项目签批form
     form_article_sub = forms.ArticleSubForm()  # 添加审批意见form
@@ -135,8 +163,11 @@ def article_scan(request, article_id):  # 项目预览
         initial={'change_date': str(datetime.date.today())})  # 变更项目form
     form_inv_add = forms.FormInvestigateAdd(
         initial={'inv_date': str(datetime.date.today())})  # 添加补充调查form
-    form_opinion = forms.FormOpinion(initial={'article_repay_method': article_obj.article_repay_method,
-                                              'opinion': article_obj.opinion})  # 添加放款次序form
+    form_opinion = forms.FormOpinion(
+        initial={
+            'article_repay_method': article_obj.article_repay_method,
+            'opinion': article_obj.opinion
+        })  # 添加放款次序form
 
     form_change_article_state = forms.ArticleStateChangeForm(
         initial={'article_state': article_obj.article_state})
@@ -144,6 +175,7 @@ def article_scan(request, article_id):  # 项目预览
 
 
 # -----------------------------项目预览-按合同------------------------------#
+
 
 @login_required
 @authority
@@ -169,6 +201,7 @@ def article_scan_agree(request, article_id, agree_id):  # 项目预览
 
 
 # -----------------------------项目预览-按发放次序------------------------------#
+
 
 @login_required
 @authority
@@ -197,66 +230,89 @@ def article_scan_lending(request, article_id, lending_id):  # 项目预览
     HOUSE_LIST = [11, 21, 42, 52]  # 房产类
     GROUND_LIST = [12, 22, 43, 53]  # 土地类
     COUNSTRUCT_LIST = [14, 23]  # 在建工程类
-    RECEIVABLE_LIST = [31, ]  # 应收账款类
+    RECEIVABLE_LIST = [
+        31,
+    ]  # 应收账款类
     STOCK_LIST = [32, 51]  # 股权类
     DRAFT_LIST = [33, 44]  # 票据类
-    VEHICLE_LIST = [15, ]  # 车辆类
+    VEHICLE_LIST = [
+        15,
+    ]  # 车辆类
     CHATTEL_LIST = [13, 24, 34, 47]  # 动产类
     OTHER_LIST = [39, 49, 59]  # 其他类
     '''反担保情况'''
     custom_lending_list = models.Customes.objects.filter(
         lending_custom__sure__lending=lending_obj)  # 放款次序下-反担保人列表
     warrant_lending_h_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ__in=[1, 2])  # 放款次序下-房产列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ__in=[1, 2])  # 放款次序下-房产列表
     warrant_lending_g_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ=5)  # 放款次序下-土地列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ=5)  # 放款次序下-土地列表
     warrant_lending_6_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ=6)  # 放款次序下-在建工程列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ=6)  # 放款次序下-在建工程列表
     warrant_lending_r_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ=11)  # 放款次序下-应收账款列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ=11)  # 放款次序下-应收账款列表
     warrant_lending_s_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ=21)  # 放款次序下-股权列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ=21)  # 放款次序下-股权列表
     warrant_lending_d_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ=31)  # 放款次序下-票据列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ=31)  # 放款次序下-票据列表
     warrant_lending_v_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ=41)  # 放款次序下-车辆列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ=41)  # 放款次序下-车辆列表
     warrant_lending_c_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ=51)  # 放款次序下-动产列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ=51)  # 放款次序下-动产列表
     warrant_lending_o_list = models.Warrants.objects.filter(
-        lending_warrant__sure__lending=lending_obj, warrant_typ=55)  # 放款次序下-其他列表
+        lending_warrant__sure__lending=lending_obj,
+        warrant_typ=55)  # 放款次序下-其他列表
     form_agree_add = forms.ArticleAgreeAddForm()  # 创建合同form
     today_str = datetime.date.today()
     date_th_later = today_str + datetime.timedelta(days=365)
-    from_letter_data = {'starting_date': str(
-        today_str), 'due_date': str(date_th_later)}
+    from_letter_data = {
+        'starting_date': str(today_str),
+        'due_date': str(date_th_later)
+    }
     form_letter_add = forms.LetterGuaranteeAddForm(
         initial=from_letter_data)  # 创建公司保函合同
-    from_jk_data = {'agree_start_date': str(today_str), 'agree_due_date': str(date_th_later),
-                    'acc_name': str(article_obj.custom.name), }
+    from_jk_data = {
+        'agree_start_date': str(today_str),
+        'agree_due_date': str(date_th_later),
+        'acc_name': str(article_obj.custom.name),
+    }
     form_agree_jk_add = forms.AgreeJkAddForm(
         initial=from_jk_data)  # 创建小贷借款合同扩展
-
     '''GENRE_LIST = [(1, '企业'), (2, '个人')]'''
     form_lendingcustoms_c_add = models.Customes.objects.exclude(
-        id=article_obj.custom.id).filter(genre=1).values_list('id', 'name')  # 除项目客户外的企业
+        id=article_obj.custom.id).filter(genre=1).values_list(
+            'id', 'name')  # 除项目客户外的企业
     form_lendingcustoms_p_add = models.Customes.objects.exclude(
-        id=article_obj.custom.id).filter(genre=2).values_list('id', 'name')  # 除项目客户外的个人
+        id=article_obj.custom.id).filter(genre=2).values_list(
+            'id', 'name')  # 除项目客户外的个人
     form_lendingsures = forms.LendingSuresForm()  # 反担保类型form
     # form_lendingcustoms_c_add = forms.LendingCustomsCForm()
     # form_lendingcustoms_p_add = forms.LendingCustomsPForm()
     form_lendinghouse_add = forms.LendingHouseForm()  # 添加（反）担保-房产form
     form_lendingground_add = forms.LendingGroundForm()  # 添加（反）担保-土地form
-    form_lendingconstruct_add = forms.LendingConstructForm()  # 添加（反）担保-在建工程form
-    form_lendinggreceivable_add = forms.LendinReceivableForm()  # 添加（反）担保-应收账款form
+    form_lendingconstruct_add = forms.LendingConstructForm(
+    )  # 添加（反）担保-在建工程form
+    form_lendinggreceivable_add = forms.LendinReceivableForm(
+    )  # 添加（反）担保-应收账款form
     form_lendingstock_add = forms.LendinStockForm()  # 添加（反）担保-股权form
     form_lendingdraft_add = forms.LendinDraftForm()  # 添加（反）担保-票据form
     form_lendingvehicle_add = forms.LendinVehicleForm()  # 添加（反）担保-车辆form
     form_lendingchattel_add = forms.LendinChattelForm()  # 添加（反）担保-动产form
     form_lendingother_add = forms.LendinOtherForm()  # 添加（反）担保-其他form
     form_lending = forms.FormLendingOrder(
-        initial={'order': lending_obj.order,
-                 'remark': lending_obj.remark,
-                 'order_amount': lending_obj.order_amount})  # 放款次序变更form
+        initial={
+            'order': lending_obj.order,
+            'remark': lending_obj.remark,
+            'order_amount': lending_obj.order_amount
+        })  # 放款次序变更form
 
     return render(request, 'dbms/article/article-scan-lending.html', locals())
 
@@ -302,28 +358,35 @@ def endor_list_scan(request, article_id):  # 签批单
                         ttt += '、'
                 ttt += '提供%s%s' % (sure_typ_dic[sure.sure_typ], '担保')
             elif sure.sure_typ in [11, 21]:  # (11, '房产抵押'), (21, '房产顺位'),
-                sure_warrant_list = sure.warrant_sure.warrant.all()  # 担保措施下权证列表
+                sure_warrant_list = sure.warrant_sure.warrant.all(
+                )  # 担保措施下权证列表
                 sure_warrant_count = sure_warrant_list.count()  # 担保措施下权证数量
                 sure_warrant_c = 0
                 for warrant in sure_warrant_list:
                     sure_warrant_c += 1
-                    owner_ship_list = warrant.ownership_warrant.all()  # 权证项下权证列表
+                    owner_ship_list = warrant.ownership_warrant.all(
+                    )  # 权证项下权证列表
                     owner_ship_count = owner_ship_list.count()  # 权证项下权证数量
                     owner_ship_c = 0
                     if owner_ship_list:
                         for owner_ship in owner_ship_list:
                             owner_ship_c += 1
-                            ttt += owner_ship.owner.name
+                            ttt += '%s(%s)' %(owner_ship.owner.name,owner_ship.ownership_num)
                             if owner_ship_c < owner_ship_count:
                                 ttt += '、'
                     else:
                         ttt += '？？？？？？？？？？？'
-                    ttt += '所有的位于%s的%s' % (warrant.house_warrant.house_locate, '住宅')
+                    ttt += '所有的位于%s的%s' % (warrant.house_warrant.house_locate,
+                                           '住宅')
                     if sure_warrant_c < sure_warrant_count:
                         ttt += '、'
                 ttt += '提供%s%s' % (sure_typ_dic[sure.sure_typ], '担保。')
             if sure_lending_c < sure_lending_count:
                 ttt += '；'
-    PROCESS_LIST_XD = ['房抵贷', '担保贷', '过桥贷', ]
+    PROCESS_LIST_XD = [
+        '房抵贷',
+        '担保贷',
+        '过桥贷',
+    ]
 
     return render(request, 'dbms/appraisal/endor-list.html', locals())
